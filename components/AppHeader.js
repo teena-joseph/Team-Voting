b7f1d10fdfa3edddf810f0df571df793dc78a943
teena-joseph <teenasijojoseph@gmail.com>
2021-12-03 19:19:21 +0530

    changed the button color

--- components/AppHeader.js
+++ components/AppHeader.js
@@ -13,7 +13,7 @@ export default class AppHeader extends React.Component{
 
 const styles = StyleSheet.create({
   textContainer:{
-    backgroundColor: '#FFC300'
+    backgroundColor: '#FFF1A2'
   },
   text:{
     padding: 20,
--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -66,7 +66,7 @@ const styles = StyleSheet.create({
     marginTop: 50,
   },
   buttons: {
-    backgroundColor:"coral",
+    backgroundColor:"grey",
     alignItems: 'center',
     justifyContent: 'center',
     borderWidth: 1,
